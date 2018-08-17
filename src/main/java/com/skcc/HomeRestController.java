07c89073c7377aa6b3c32cb941aab2c6e3867ac9
yunsangjun <ysj188cm@gmail.com>
2018-08-17 16:51:06 +0900

    Init

---
+++ src/main/java/com/skcc/HomeRestController.java
@@ -0,0 +1,15 @@
+package com.skcc;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeRestController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String getHome() {
+		return "Hello prod application!.";
+	}
+
+}
---
+++ src/main/java/com/skcc/SpringBootCicdDemoApplication.java
@@ -0,0 +1,12 @@
+package com.skcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCicdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCicdDemoApplication.class, args);
+	}
+}
